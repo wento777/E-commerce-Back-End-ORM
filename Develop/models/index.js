6646b5add3a0ddbc0f310bfb6860496cdf4e3501
wento <florea_andreiwento@yahoo.com>
2023-03-15 23:25:10 -0500

    added new index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,14 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasMany(product)
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongToMany(Tag , {
+  trough: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
+
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foriegnKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
